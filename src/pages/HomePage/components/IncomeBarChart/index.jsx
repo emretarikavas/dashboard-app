adeb5e4833fa43d1ffe14c415d0e48016d8461da
Emre Tarık Kavas <67765353+emretarikavas@users.noreply.github.com>
2023-09-26 13:06:15 +0300

    update IncomeBarChart

--- src/pages/HomePage/components/IncomeBarChart/index.jsx
+++ src/pages/HomePage/components/IncomeBarChart/index.jsx
@@ -1,15 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Bar, BarChart, ResponsiveContainer, Tooltip } from "recharts";
 import "./incomeBarChart.scss";
 import { billingData } from "src/data";
 import { UserContext } from "src/context/UserContext";
 
 function index() {
-  const { department } = useContext(UserContext);
+  const { department, dateRange } = useContext(UserContext);
+  const [filteredData, setFilteredData] = useState([]);
 
-  const filteredData = billingData.filter(
-    (bill) => bill.status === "Gelir" && bill.department === department
-  );
+  useEffect(() => {
+    const newFilteredData = billingData.filter((bill) => {
+      const billDate = new Date(bill.date);
+      return (
+        bill.status === "Gelir" &&
+        bill.department === department &&
+        billDate >= dateRange.startDate &&
+        billDate <= dateRange.endDate
+      );
+    });
+
+    setFilteredData(newFilteredData);
+  }, [department, dateRange]);
 
   return (
     <div className="barChartBox">
